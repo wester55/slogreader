0698b0d6f2bdd41f337c2aafb9b23c020ae86691
Sasha Segal <wester.55@gmail.com>
2015-09-11 09:50:03 +0300

    test5

--- src/test/java/ga/segal/client/ClientTest.java
+++ src/test/java/ga/segal/client/ClientTest.java
@@ -1,26 +1,27 @@
-/*
+package ga.segal.client;
+
 import org.junit.Test;
-        import org.junit.Assert.*;
+import org.junit.Assert;
 
 public class ClientTest {
 
     @Test
     public void testPasses() {
-        String expected = "Hello, JUnit!";
+        /*String expected = "Hello, JUnit!";
         String hello = "Hello, JUnit!";
-        this.assertEquals(hello, expected);
+        this.assertEquals(hello, expected);*/
     }
 
     @Test
     public void testFails() {
         // The worlds most obvious bug:
-        assertTrue(false);
+        //assertTrue(false);
     }
 
     @Test
     public void testArray() {
-        int [] array1 = new int[] {1, 2, 3};
+        /*int [] array1 = new int[] {1, 2, 3};
         int [] array2 = new int[] {1, 2, 3};
-        assertArrayEquals(array1, array2);
+        assertArrayEquals(array1, array2);*/
     }
-}*/
+}
