4e602fecc06eb0fbec7e627465073e1166567989
Sasha Segal <wester.55@gmail.com>
2015-09-11 10:20:22 +0300

    test5

--- src/main/java/ga/segal/client/Client.java
+++ src/main/java/ga/segal/client/Client.java
@@ -11,20 +11,20 @@ public class Client {
 
     public static void main(String[] args) {
 
-        Runtime.getRuntime().addShutdownHook(new ShutdownHook());
+        //Runtime.getRuntime().addShutdownHook(new ShutdownHook());
 
         Config cf = new Config();
         try {
             cf.GetConfig();
         } catch (IOException ex){
-            System.out.println(ex.getMessage());
+            System.out.printf("Unable to find config, stack trace: %s%n", ex.getMessage());
             System.exit(1);
         }
         int interval = cf.GetInterval() * 60;
         String path = cf.GetPath();
         System.out.printf("Running on: %s at interval %d seconds%n", path,interval);
 
-        int i = 0;
+        /*int i = 0;
         while (running) {
             System.out.println("count=" + i);
             i++;
@@ -33,7 +33,7 @@ public class Client {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }
+        }*/
     }
 
 }
